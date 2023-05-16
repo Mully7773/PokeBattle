d699b460304f1daf9a41ae2af95bf3e886e8a611
Nick M <mully7773@gmail.com>
2023-05-16 13:39:33 -0500

    Successfully map out Pokemon type icons dynamically

--- src/components/PokeCard/CardHeader.jsx
+++ src/components/PokeCard/CardHeader.jsx
@@ -1,17 +1,24 @@
 const CardHeader = ({ pokemon }) => {
+  const pokemonTypes = pokemon.types.map(type => type.type.name);
+  console.log(pokemonTypes);
   return (
     <header className='mx-5 flex'>
       <h1 className='capitalize font-bold text-lg'>{pokemon?.name}</h1>
       <div className='ml-auto uppercase flex gap-x-1 font-bold font-secondary text-lg text-red-600 items-center justify-center'>
         <span>{pokemon?.stats[0].base_stat}</span>
         <span>{pokemon?.stats[0].stat.name}</span>
-        <span>
-          <img
-            className='w-4 mb-1'
-            src='/src/assets/pokemonTypes/fire.svg'
-            alt='Pokemon type icon'
-          />
-        </span>
+        <div className='flex'>
+          {pokemonTypes.map(type => {
+            return (
+              <img
+                key={type}
+                className='w-4 mb-1'
+                src={`/src/assets/pokemonTypes/${type}.svg`}
+                alt='Pokemon type icon'
+              />
+            );
+          })}
+        </div>
       </div>
     </header>
   );

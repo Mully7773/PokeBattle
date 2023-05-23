88dd190b348f575d5cb108aac66d2e9e9585e6fe
Nick M <mully7773@gmail.com>
2023-05-22 21:10:18 -0500

    Begin adding more color backgrounds for Pokemon types

--- src/components/PokeCard/CardBackground.jsx
+++ src/components/PokeCard/CardBackground.jsx
@@ -1,9 +1,15 @@
 const getColorByPokemonType = type => {
   switch (type) {
     case 'bug':
-      return 'rgba(167, 182, 32, 0.45)';
+      return 'rgba(118, 129, 25, 0.40)';
+    case 'dark':
+      return 'rgba(68, 104, 94, 0.40)';
+    case 'dragon':
+      return 'rgba(73, 36, 161, 0.40)';
+    case 'electric':
+      return 'rgb(250, 224, 120, 0.40)';
     case 'steel':
-      return 'rgba(114, 121, 126, 0.45)';
+      return 'rgba(75, 79, 83, 0.40)';
     default:
       return '#71797E';
   }
